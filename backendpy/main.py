6885414e7e0645c23702fc1ff6f8bf46dab0b3f0
zunaira malik <malikzunaira118@gmail.com>
2026-01-08 14:32:06 +0500

    Update CORS origins for the backend

--- backendpy/main.py
+++ backendpy/main.py
@@ -16,7 +16,7 @@ DATABASE_URL = "postgresql://neondb_owner:npg_ciwE76UuTKdS@ep-empty-wave-a1qdt4l
 # Allow CORS for React Frontend
 origins = [
     "http://localhost:5173",           
-    "https://smartgrid_fyp.onrender.com" 
+    "https://smartgridx.onrender.com" 
 ]
 
 app.add_middleware(
